964eeded11703ffb0879dc6bc86bf9d0c43a6b16
marciovz <marciovz@yahoo.com.br>
2022-02-02 15:29:35 -0300

    Valiaçao do formulario de signin

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import { Flex, Button, Stack } from '@chakra-ui/react';
-import { useForm, SubmitHandler, useFormState } from 'react-hook-form';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
 
 import { Input } from '../components/Form/Input';
 
@@ -8,8 +10,16 @@ type SignInFormData = {
   passwrod: string;
 }
 
+const signInFormSchema = yup.object({
+  email: yup.string().required('E-mail obrigatório').email('E-mail invalído'),
+  password: yup.string().required('Senha obrigatória'),
+})
+
 export default function SignIn() {
-  const { register, handleSubmit, formState } = useForm();
+  const { register, handleSubmit, formState } = useForm({
+    resolver: yupResolver(signInFormSchema),
+  });
+  const { errors } = formState;
 
   const handleSignIn: SubmitHandler<SignInFormData> = async (values) => {
     await new Promise(resolve => setTimeout(resolve, 2000));
@@ -35,8 +45,20 @@ export default function SignIn() {
         onSubmit={handleSubmit(handleSignIn)}
       >
         <Stack spacing="4">
-          <Input name="email" type="email" label="E-mail" {...register('email')} />
-          <Input name="password" type="password" label="Senha" {...register('password')} />
+          <Input
+            name="email" 
+            type="email" 
+            label="E-mail" 
+            error={errors.email}
+            {...register('email')} 
+          />
+          <Input
+            name="password" 
+            type="password" 
+            label="Senha" 
+            error={errors.password}
+            {...register('password')} 
+          />
 
         </Stack>
 
