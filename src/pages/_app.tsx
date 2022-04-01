e2e8647bcdf7d52906d8d313c93a7a03a7942de6
marciovz <marciovz@yahoo.com.br>
2022-04-01 16:02:11 -0300

    Configurando React Query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
+import { QueryClientProvider, QueryClient } from 'react-query';
+
 import { theme } from '../styles/theme';
 import { makeServer } from '../services/mirage';
 
@@ -9,13 +11,17 @@ if (process.env.NODE_ENV === 'development') {
   makeServer();
 }
 
+const queryClient = new QueryClient();
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <SidebarDrawerProvider>
-        <Component {...pageProps} />
-      </SidebarDrawerProvider>
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <SidebarDrawerProvider>
+          <Component {...pageProps} />
+        </SidebarDrawerProvider>
+      </ChakraProvider>
+    </QueryClientProvider>
   )
 }
 
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,8 +1,9 @@
 import { useEffect } from 'react';
 import Link from 'next/link';
 import { Box, Flex, Heading, Button, Checkbox, Icon, Text, Table, Thead, 
-  Tr, Th, Tbody, Td , useBreakpointValue } from '@chakra-ui/react';
+  Tr, Th, Tbody, Td , useBreakpointValue, Spinner } from '@chakra-ui/react';
 import { RiAddLine, RiPencilLine } from 'react-icons/ri';
+import { useQuery } from 'react-query';
 
 import { Header } from '../../components/Header';
 import { Sidebar } from '../../components/Sidebar';
@@ -10,16 +11,20 @@ import { Pagination } from '../../components/Pagination';
 
 
 export default function UserList() {
+
+  const { data, isLoading, error } = useQuery('users', async () => {
+    const response = await fetch('http://localhost:3000/api/users');
+    const data = await response.json();
+    
+    return data;
+  })
+
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
   });
 
-  useEffect(() => {
-    fetch('http://localhost:3000/api/users')
-    .then(response => response.json())
-    .then(data => console.log(data))
-  }, []);
 
   return (
     <Box>
@@ -45,100 +50,113 @@ export default function UserList() {
             </Link>
           </Flex>
 
-          <Table colorScheme="whiteAlpha">
-            <Thead>
-              <Tr>
-                <Th px={["4", "4", "6"]} color="gray.300" width="8">
-                  <Checkbox colorScheme="pink" />
-                </Th>
-                <Th>Usuário</Th>
-                { isWideVersion && <Th>Data de cadastro</Th>}
-                { isWideVersion && <Th width="8"></Th> }
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold" >Diego Fernandes</Text>
-                    <Text fontSize="sm" color="gray.300">diego.schell.f@gmail.com</Text>
-                  </Box>
-                </Td>
-                { isWideVersion && <Td>04 de Abril, 2021</Td> }
-                { isWideVersion && (
-                  <Td>
-                    <Button
-                      as="a"
-                      size="sm"
-                      fontSize="sm"
-                      colorScheme="purple"
-                      leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                    >
-                      Editar
-                    </Button>
-                  </Td>
-                )} 
-              </Tr>
+          { isLoading ? (
+            <Flex justify="center">
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Flex justiry="center">
+              <Text>Fahla ao obter ddos dos usuários.</Text>
+            </Flex>
+          ) : (
+            <>
+              <Table colorScheme="whiteAlpha">
+                <Thead>
+                  <Tr>
+                    <Th px={["4", "4", "6"]} color="gray.300" width="8">
+                      <Checkbox colorScheme="pink" />
+                    </Th>
+                    <Th>Usuário</Th>
+                    { isWideVersion && <Th>Data de cadastro</Th>}
+                    { isWideVersion && <Th width="8"></Th> }
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  <Tr>
+                    <Td px={["4", "4", "6"]}>
+                      <Checkbox colorScheme="pink" />
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text fontWeight="bold" >Diego Fernandes</Text>
+                        <Text fontSize="sm" color="gray.300">diego.schell.f@gmail.com</Text>
+                      </Box>
+                    </Td>
+                    { isWideVersion && <Td>04 de Abril, 2021</Td> }
+                    { isWideVersion && (
+                      <Td>
+                        <Button
+                          as="a"
+                          size="sm"
+                          fontSize="sm"
+                          colorScheme="purple"
+                          leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                        >
+                          Editar
+                        </Button>
+                      </Td>
+                    )} 
+                  </Tr>
 
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold" >Diego Fernandes</Text>
-                    <Text fontSize="sm" color="gray.300">diego.schell.f@gmail.com</Text>
-                  </Box>
-                </Td>
-                { isWideVersion && <Td>04 de Abril, 2021</Td> }
-                { isWideVersion && (
-                  <Td>
-                    <Button
-                      as="a"
-                      size="sm"
-                      fontSize="sm"
-                      colorScheme="purple"
-                      leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                    >
-                      Editar
-                    </Button>
-                  </Td>
-                )} 
-              </Tr>
+                  <Tr>
+                    <Td px={["4", "4", "6"]}>
+                      <Checkbox colorScheme="pink" />
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text fontWeight="bold" >Diego Fernandes</Text>
+                        <Text fontSize="sm" color="gray.300">diego.schell.f@gmail.com</Text>
+                      </Box>
+                    </Td>
+                    { isWideVersion && <Td>04 de Abril, 2021</Td> }
+                    { isWideVersion && (
+                      <Td>
+                        <Button
+                          as="a"
+                          size="sm"
+                          fontSize="sm"
+                          colorScheme="purple"
+                          leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                        >
+                          Editar
+                        </Button>
+                      </Td>
+                    )} 
+                  </Tr>
 
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold" >Diego Fernandes</Text>
-                    <Text fontSize="sm" color="gray.300">diego.schell.f@gmail.com</Text>
-                  </Box>
-                </Td>
-                { isWideVersion && <Td>04 de Abril, 2021</Td> }
-                { isWideVersion && (
-                  <Td>
-                    <Button
-                      as="a"
-                      size="sm"
-                      fontSize="sm"
-                      colorScheme="purple"
-                      leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                    >
-                      Editar
-                    </Button>
-                  </Td>
-                )}  
-              </Tr>
+                  <Tr>
+                    <Td px={["4", "4", "6"]}>
+                      <Checkbox colorScheme="pink" />
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text fontWeight="bold" >Diego Fernandes</Text>
+                        <Text fontSize="sm" color="gray.300">diego.schell.f@gmail.com</Text>
+                      </Box>
+                    </Td>
+                    { isWideVersion && <Td>04 de Abril, 2021</Td> }
+                    { isWideVersion && (
+                      <Td>
+                        <Button
+                          as="a"
+                          size="sm"
+                          fontSize="sm"
+                          colorScheme="purple"
+                          leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                        >
+                          Editar
+                        </Button>
+                      </Td>
+                    )}  
+                  </Tr>
 
-            </Tbody>
-          </Table>
+                </Tbody>
+              </Table>
 
-          <Pagination />
+              <Pagination />
+            </>
+          )}
+
         </Box>
 
       </Flex>
