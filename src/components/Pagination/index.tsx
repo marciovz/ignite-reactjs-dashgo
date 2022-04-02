cfbea893511d9fcd023fe99cb37077f25861e32c
marciovz <marciovz@yahoo.com.br>
2022-04-02 15:38:55 -0300

    Trocando de página

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -52,7 +52,7 @@ export function Pagination({
 
         {currentPage > (1 + siblingsCount) && (
           <>
-            <PaginationItem number={1} />
+            <PaginationItem number={1} onPageChange={onPageChange} />
             {currentPage > (2 + siblingsCount) && (
               <Text color="gray.300" width="8" textAlign="center">...</Text>
             )}
@@ -60,13 +60,13 @@ export function Pagination({
         )}
         
         {previousPages.length > 0 && previousPages.map(page => {
-          return <PaginationItem key={page} number={page} />
+          return <PaginationItem key={page} number={page} onPageChange={onPageChange} />
         })}
         
-        <PaginationItem number={currentPage} isCurrent />
+        <PaginationItem number={currentPage} onPageChange={onPageChange} isCurrent />
 
         {nextPages.length > 0 && nextPages.map(page => {
-          return <PaginationItem key={page} number={page} />
+          return <PaginationItem key={page} number={page} onPageChange={onPageChange} />
         })}
 
         {(currentPage + siblingsCount) < lastPage && (
@@ -74,7 +74,7 @@ export function Pagination({
           { (currentPage + 1 + siblingsCount) < lastPage && (
             <Text color="gray.300" width="8" textAlign="center">...</Text>
           )}
-          <PaginationItem number={lastPage} />
+          <PaginationItem number={lastPage} onPageChange={onPageChange} />
         </>
         )}
         
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Link from 'next/link';
 import { Box, Flex, Heading, Button, Checkbox, Icon, Text, Table, Thead, 
   Tr, Th, Tbody, Td , useBreakpointValue, Spinner } from '@chakra-ui/react';
@@ -10,8 +11,8 @@ import { useUsers } from '../../services/hooks/useUsers';
 
 
 export default function UserList() {
-
-  const { data, isLoading, isFetching, error } = useUsers();
+  const [page, setPage] = useState(1)
+  const { data, isLoading, isFetching, error } = useUsers(page);
 
   const isWideVersion = useBreakpointValue({
     base: false,
@@ -69,7 +70,7 @@ export default function UserList() {
                   </Tr>
                 </Thead>
                 <Tbody>
-                  {data.map(user => {
+                  {data.users.map(user => {
                     return (
                       <Tr key={user.id}>
                         <Td px={["4", "4", "6"]}>
@@ -102,11 +103,11 @@ export default function UserList() {
               </Table>
 
               <Pagination 
-                totalCountOfRegisters={200}
-                currentPage={5}
-                onPageChange={() => {}}
+                totalCountOfRegisters={data.totalCount}
+                currentPage={page}
+                onPageChange={setPage}
               />
-              
+
             </>
           )}
         </Box>
